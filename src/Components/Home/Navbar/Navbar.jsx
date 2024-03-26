6bfb202c2bbc59b8bc7d9c18e7fa0356eaf29798
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-03-26 17:30:57 +0600

    first commit

---
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to='/'>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to='/listedBooks'>Listed Books</NavLink>
+      </li>
+      <li>
+        <NavLink to='/pagesToRead'>Pages to read</NavLink>
+      </li>
+      <li>
+        <NavLink to='/category'>Category</NavLink>
+      </li>
+      <li>
+        <NavLink to='/bookmarks'>Bookmarks</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-xl fixd">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52 gap-1"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn  text-2xl font-bold">Book Vibe</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-2">{links}</ul>
+      </div>
+      <div className="navbar-end ">
+        <a className="btn mr-2">Sign In</a>
+        <a className="btn">Sign Up</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import ErrorPage from './Components/Home/ErrorPage/ErrorPage';
+import ListedBooks from './Components/Home/ListedBoooks/ListedBooks';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Home/Root/Root';
+import Home from './Components/Home/Home';
+import PagesToRead from './Components/Home/PagesToRead/PagesToRead';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/listedBooks",
+        element:<ListedBooks></ListedBooks>,
+      },
+      {
+        path: "/pagesToRead",
+        element:<PagesToRead></PagesToRead>,
+      },
+      {
+        path: "/category",
+        element: <category></category>,
+      },
+      {
+        path: "/bookmarks",
+        element:<bookmark></bookmark>
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
